45a0af9a350b5a6746c75ab638ab5c4a711fd87e
Zhenhua yu <zyu0628@gmail.com>
2020-10-13 21:51:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function total() {
 function removeFromCart(item) {
   for (let i=0; i<cart.length;i++) {
     if(item===cart[i].itemName){
-    cart.splice(0,i)
+    cart.splice(i,1)
   }
 }return "That item is not in your cart."
 }
