780d5ae051240987010132c1a2f97c26fa8b678c
Zhenhua yu <zyu0628@gmail.com>
2020-10-13 22:30:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber.length===0){
+  if (cardNumber===0){
     return "Sorry, we don't have a credit card on file for you."
   }else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
