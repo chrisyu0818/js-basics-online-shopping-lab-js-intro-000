5bed4080bc648035fcf7cc88cc8d3eb79ab3e0f4
Zhenhua yu <zyu0628@gmail.com>
2020-10-13 22:38:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-  }return cart.pop()
+  }return cart.splice(0)
 }
