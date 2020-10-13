11646e4fbd256038a2f54367368e33b9bfc616f0
Zhenhua yu <zyu0628@gmail.com>
2020-10-13 22:33:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,5 +59,6 @@ function placeOrder(cardNumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }while (cart.length>0){
     cart.pop()
+    cart++
   }return cart
 }
