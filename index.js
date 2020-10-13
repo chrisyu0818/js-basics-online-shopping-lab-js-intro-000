07f062a64013d393fa0e774708a09a31e7568e63
Zhenhua yu <zyu0628@gmail.com>
2020-10-13 17:27:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var listItem = {
-   itemName:item,
-   itemPrice:math.floor(math.random()*101)};
+ var price = math.floor(math.random()*101)
+ var listItem = {itemName:item, itemPrice:price};
    cart.push(listItem);
    return `${item} has been added to your cart.`
 }
+
 function viewCart() {
   // write your code here
 }
