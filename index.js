ac9782cf317ec03f5559d6830d0b69e164a3573d
Zhenhua yu <zyu0628@gmail.com>
2020-10-14 00:15:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ function placeOrder(cardNumber) {
   if (!cardNumber){
     return "Sorry, we don't have a credit card on file for you."
   }else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    let message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     cart.splice(0, cart.length)
+    return message
   }
 }
